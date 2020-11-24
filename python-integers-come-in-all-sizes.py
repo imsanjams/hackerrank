0c97a91d69c9df77020857c4d2960964861371d2
Sanjam Singh <47633733+ssanjam20@users.noreply.github.com>
2020-11-24 21:11:02 +0530

    Delete python-integers-come-in-all-sizes.py

--- python-integers-come-in-all-sizes.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-if __name__ == "__main__":
-    a = int(input().strip())
-    b = int(input().strip())
-    c = int(input().strip())
-    d = int(input().strip())
-    
-    print(pow(a, b) + pow(c, d))
