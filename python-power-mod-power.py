cf4b13238361b41d05212f852b7c9dbcf8db69a1
Sanjam Singh <47633733+ssanjam20@users.noreply.github.com>
2020-11-24 21:11:48 +0530

    Delete python-power-mod-power.py

--- python-power-mod-power.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-if __name__ == "__main__":
-    a = int(input().strip())
-    b = int(input().strip())
-    m = int(input().strip())
-    
-    print(pow(a, b))
-    print(pow(a, b, m))
